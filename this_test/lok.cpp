4b283c2022c41e89f9889df7bd52f43cfb0ed8e3
LR <LR@DESKTOP-8BNI9L6>
2018-10-24 11:50:00 +0200

    first commit

---
+++ this_test/lok.cpp
@@ -0,0 +1,18 @@
+/*
+ * lok.cpp
+ *
+ *  Created on: 24 oct. 2018
+ *      Author: LR
+ */
+
+#include "lok.h"
+
+lok::lok() {
+	// TODO Auto-generated constructor stub
+
+}
+
+lok::~lok() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ this_test/lok.h
@@ -0,0 +1,17 @@
+/*
+ * lok.h
+ *
+ *  Created on: 24 oct. 2018
+ *      Author: LR
+ */
+
+#ifndef LOK_H_
+#define LOK_H_
+
+class lok {
+public:
+	lok();
+	virtual ~lok();
+};
+
+#endif /* LOK_H_ */
