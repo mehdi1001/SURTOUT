817ecfd13b0b1ba99fcf77950e77f52ad19876da
LR <LR@DESKTOP-8BNI9L6>
2018-10-24 11:51:56 +0200

    second commit

---
+++ this_test/nowstuff.cpp
@@ -0,0 +1,18 @@
+/*
+ * nowstuff.cpp
+ *
+ *  Created on: 24 oct. 2018
+ *      Author: LR
+ */
+
+#include "nowstuff.h"
+
+now_stuff::now_stuff() {
+	// TODO Auto-generated constructor stub
+
+}
+
+now_stuff::~now_stuff() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ this_test/nowstuff.h
@@ -0,0 +1,17 @@
+/*
+ * nowstuff.h
+ *
+ *  Created on: 24 oct. 2018
+ *      Author: LR
+ */
+
+#ifndef NOWSTUFF_H_
+#define NOWSTUFF_H_
+
+class now_stuff {
+public:
+	now_stuff();
+	virtual ~now_stuff();
+};
+
+#endif /* NOWSTUFF_H_ */
